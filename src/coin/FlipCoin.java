3ba89a82a7a10e78e61aa56f464ec7345c5e9a44
vik554 <kumbharvikram27@gmail.com>
2023-05-14 20:55:16 +0530

    ADDED code to Filp coin

---
+++ src/coin/FlipCoin.java
@@ -0,0 +1,13 @@
+package coin;
+import java.util.Random:
+
+public class FlipCoin {
+    public static void main(String[] args) {
+
+        int numFlips = 10;
+        int heads = 0;
+
+        Random rand = new Random();
+        for (int i = 0; i < numFlips; i++) ;
+            }
+        }
---
+++ src/coin/LeapYear.java
@@ -0,0 +1,16 @@
+package coin;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a leap year: ");
+        int leapyear = scanner.nextInt();
+
+        if (isLeapYear) {
+            System.out.println(leapyear + " leap year.");
+        } else {
+            System.out.println(leapyear + "  not a leap year.");
+        }
+    }
+}
