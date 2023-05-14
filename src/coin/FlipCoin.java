51b73b6264ecf42925722500e4b1a8bd52f488ba
vik554 <kumbharvikram27@gmail.com>
2023-05-14 21:21:33 +0530

    ADDED: code to leap year

--- src/coin/FlipCoin.java
+++ src/coin/FlipCoin.java
@@ -1,13 +1,32 @@
 package coin;
 import java.util.Random:
+import java.util.Scanner;
 
 public class FlipCoin {
     public static void main(String[] args) {
+        FlipCoin s1 = new FlipCoin();
 
+
+
+
+    }
+    public void flipcoin() {
         int numFlips = 10;
         int heads = 0;
 
         Random rand = new Random();
         for (int i = 0; i < numFlips; i++) ;
-            }
+
+    }
+
+
+    public void LeapYear() {
+        int leapyear = scanner.nextInt();
+
+        if (isLeapYear) {
+            System.out.println(leapyear + " leap year.");
+        } else {
+            System.out.println(leapyear + "  not a leap year.");
         }
+    }
+}
